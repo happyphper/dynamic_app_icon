bf8276d7635d5f78e8115381bff29a56d18f9670
王宝龙 <282584778@qq.com>
2021-07-14 17:07:29 +0800

    fix(test): 测试脚本调整

--- test/dynamic_app_icon_test.dart
+++ test/dynamic_app_icon_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dynamic_app_icon/dynamic_app_icon.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('dynamic_app_icon');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await DynamicAppIcon.platformVersion, '42');
   });
 }
